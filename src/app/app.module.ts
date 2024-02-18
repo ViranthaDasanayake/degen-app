10c23a7560203e88f29f47ee6821d8be90489540
Virantha Dasanayake <viranthanavhdasanayake@gmail.com>
2024-02-18 22:12:56 +0530

    Added mobile responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
+  providers: [MessageService],
 })
 export class MainComponent {
-  constructor() {}
+  @Input('appCopyToClipboard') text?: string;
+  constructor(private messageService: MessageService) {}
 
   ngOnInit() {}
   navigate(link: any) {
     window.open(link, '_blank');
+  }
+
+  onCopy(code: string) {
+    const el = document.createElement('textarea');
+    el.value = code;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    this.messageService.add({
+      severity: 'success',
+      summary: '',
+      detail: 'Copied to clipboard',
+    });
   }
 }
